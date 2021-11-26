b0f39cbe2ddbd6b66fbbfc50374609b7357ace33
gyaanap51 <85648046+gyaanap51@users.noreply.github.com>
2021-11-26 11:56:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,61 +1,13 @@
-video = "";
-objects=[];
-status = "";
-function preload(){
-video = createVideo('video.mp4');
-video.hide();
-}
 function setup(){
-    canvas = createCanvas(640, 480);
-    canvas.center();
-}
-function draw(){
-    image(video, 0, 0, 640, 480);
-    if(status !="")
-    {
-        objectDetector.detect(video, gotResult);
-        for (i = 0; i< objects.length; i++) {
-            document.getElementById("status").innerHTML = "Status : Objects detected";
-            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are :"+ objects.length;
+  var canvas =  createCanvas(700,600);
+  canvas.center();
+  
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
 
-          r=random(255);
-          g=random(255);
-          b=random(255);
+  }
+  
+function preload(){
 
-
-
-        fill(r, g, b);
-        percent = floor(objects[i].confidence * 100);
-        text(objects[i].label + " "+ percent + "%", objects[i].x + 15, objects[i].y + 15);
-        noFill();
-        stroke(r, g, b);
-        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
-        }
-    }
-}
-
-
-function gotResult(error, results){
-    if(error){
-        console.log(error);
-    }
-    console.log(results);
-    objects = results;
-
-}
-
-function start()
-{
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
-}
-
-function modelLoaded()
-    {
- console.log("Model Loaded!");
- status = true;
- video.loop();
- video.speed(1);
- video.volume(0);
-
-    }
+  }
